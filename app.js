d40e776979a23b8a727b43b2babd7e1bdc3d32e1
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-07-21 16:01:13 +0200

    Sending requests & responses and working wirg Postman

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const feedRoutes = require('./routes/feed')
-const app = express()
+const app = express() 
+
+// app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
+app.use(bodyParser.json()) // application/json
 
 // GET /feed/posts
 app.use('/feed', feedRoutes)
--- routes/feed.js
+++ routes/feed.js
@@ -2,6 +2,10 @@ const express = require('express')
 const feedControler = require('../controllers/feed')
 const router = express.Router()
 
+// GET /feed/posts
 router.get('/posts', feedControler.getPosts)
+
+// POST /feed/post
+router.post('/post', feedControler.createPost)
 
 module.exports = router
