790776e03d9c2e2effc86c5cf40214b1bc5b2213
Eleonora Kazakova <eleonora.kazakova89@gmail.com>
2026-07-22 11:16:10 +0200

    Sending POST request

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json()) // application/json
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
@@ -16,3 +16,23 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes)
 
 app.listen(8080)
+
+/*
+const getButton = document.getElementById('get')
+const postButton = document.getElementById('post')
+
+getButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/posts')
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err))
+})
+
+===
+<body>
+    <button id='get'>Get Posts</button>
+    <button id='post'>Create a Post</button>
+    
+   <script src="./script.js"></script>
+
+</body>*/
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
+    console.log('req: ', req.body)
     const title = req.body.title
     const content = req.body.content
 
@@ -13,3 +14,38 @@ exports.createPost = (req, res, next) => {
     })
 
 }
+
+/* 
+codepen:
+
+const getButton = document.getElementById('get')
+const postButton = document.getElementById('post')
+
+getButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/posts')
+    .then(res => res.json())
+    .then(resData => console.log('getButton: ', resData))
+    .catch(err => console.log(err))
+})
+
+
+postButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/post', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'A Codepen Post',
+      content: 'Created via Codepen'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      res.json()
+      console.log('res: ', res)
+      
+    })
+    .then(resData => console.log('postButton_log: ', resData))
+    .catch(err => console.log('err: ', err))
+})
+*/
